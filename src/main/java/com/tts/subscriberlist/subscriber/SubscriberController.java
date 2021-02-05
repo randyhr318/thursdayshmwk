c3fe1d242ad8ea4b21b6a07b30a0a2caa26abe8f
randyhr318 <74457618+randyhr318@users.noreply.github.com>
2021-02-05 14:40:46 -0600

    Initial commit

---
+++ src/main/java/com/tts/subscriberlist/subscriber/SubscriberController.java
@@ -0,0 +1,34 @@
+package com.tts.subscriberlist.subscriber;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class SubscriberController {
+
+    @Autowired
+    private SubscriberRepository subscriberRepository;
+
+
+    @GetMapping(value = "/")
+    public String index(Subscriber subscriber) {
+        //Where we want to go when out application is started
+        return "subscriber/index";
+
+
+    }
+    private Subscriber subscriber;
+    @PostMapping(value="/")
+    public String addNewSubscriber(Subscriber subscriber, Model model){
+        subscriberRepository.save(new Subscriber(subscriber.getFirstName(),subscriber.getLastName(),subscriber.getUserName(),subscriber.getSignedUp()));
+        model.addAttribute("firstName", subscriber.getFirstName());
+        model.addAttribute("lastName", subscriber.getLastName());
+        model.addAttribute("userName", subscriber.getUserName());
+        return "subscriber/result";
+
+    }
+
+}
